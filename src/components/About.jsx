8bd002f844ce1e5d1892830d0239e1e2512ee5cc
Praneeth Saranga <praneethskulathilaka@gmail.com>
2024-09-03 03:03:08 +0530

    About

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ABOUT_TEXT } from '../constants'
+import aboutImage from '../assets/about.jpg'
+
+const About = () => {
+  return (
+    <div className='border-b border-neutral-900 pb-4 lg:mb-4'>
+        <h1 className='my-20 text-center text-4xl'>
+            About
+            <span className='text-neutral-700'> Me</span>
+        </h1>
+        <div className='flex flex-wrap'>
+            <div className='w-full lg:w-1/2 lg:p-8'>
+                <div className='flex items-center justify-center'>
+                    <img className='rounded-2xl' src={aboutImage} alt="About Image" />
+                </div>
+            </div>
+            <div className='w-full lg:w-1/2'>
+                <div className='flex justify-center lg:justify-start'>
+                    <p className='my-2 max-w-xl py-6 text-justify'>{ABOUT_TEXT}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
