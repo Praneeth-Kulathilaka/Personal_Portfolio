b823b90f92c97f7bf2aeeb65defd7a95781e05e3
Praneeth Saranga <praneethskulathilaka@gmail.com>
2024-09-02 23:40:27 +0530

    Initialized portfolio image

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            img
+        </div>
+        <div>
+            list
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
