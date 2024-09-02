a50e80b655a86f021615c63cda64436506910fd7
Praneeth Saranga <praneethskulathilaka@gmail.com>
2024-09-03 02:39:00 +0530

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { HERO_CONTENT } from '../constants'
+import prophoto from '../assets/kevinRushProfile.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='border-b border-neutral-900 pb-4 lg:mb-4'>
+        <div className='flex flex-wrap'>
+            <div className='w-full lg:w-1/2'>
+                <div className='flex flex-col items-center lg:items-start'>
+                    <h1 className='pb-16 text-4xl font-thin tracking-tight lg:mt-12 lg:text-6xl'>Praneeth Kulathilaka</h1>
+                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Software Developer</span>
+                    <p className='text-justify my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
+                </div>
+            </div>
+            <div className='w-full lg:w-1/2 lg:p-8'>
+                <div className='flex justify-center'>
+                    <img src={prophoto} alt="Praneeth Kulathilaka" />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
