ccad14bb99f619cb5eced0aec3f732578bf31f88
Praneeth Saranga <praneethskulathilaka@gmail.com>
2024-09-07 13:52:16 +0530

    New

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,28 +18,32 @@ const Contact = () => {
             className='my-10 text-center text-4xl'>Get In Touch
         </motion.h1>
         <div className='flex flex-wrap justify-between'>
-            <div className='border border-neutral-900 w-full items-center lg:w-1/2 lg:items-start lg:left-0'>
-                <ContactForm />
-            </div>
+            <motion.div 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity:0, x: -100 }}
+                transition={{ duration: 0.5, }}
+                className='border border-neutral-900 w-full py-1 items-center lg:w-1/2 lg:items-start lg:left-0'>
+                    <ContactForm />
+            </motion.div>
             <div className='text-center tracking-tighter w-full lg:w-1/2'>
                 <motion.p 
-                    whileInView={{ opacity: 1, y: 0 }}
-                    initial={{ opacity:0, y: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity:0, x: 100 }}
                     transition={{ duration: 0.5, delay: 0.25 }}
                     className='my-4'>{CONTACT.address}</motion.p>
                 <motion.p 
-                    whileInView={{ opacity: 1, y: 0 }}
-                    initial={{ opacity:0, y: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity:0, x: 100 }}
                     transition={{ duration: 0.5, delay: 0.5 }}
                     className='my-4'>{CONTACT.phoneNo}</motion.p>
                 <motion.a 
-                    whileInView={{ opacity: 1, y: 0 }}
-                    initial={{ opacity:0, y: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity:0, x: 100 }}
                     transition={{ duration: 0.5, delay: 0.75 }}
                     href="#" >{CONTACT.email}</motion.a>
                 <motion.div             
-                    whileInView={{ opacity: 1, y: 0 }}
-                    initial={{ opacity:0, y: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity:0, x: 100 }}
                     transition={{ duration: 0.5, delay: 0.75 }}
                     className='m-8 flex items-center justify-center gap-4 text-2xl'>
                     <a href="https://www.linkedin.com/in/praneeth-kulathilaka"><FaLinkedin/></a>
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,23 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const ContactForm = () => {
+    const [name,setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        const service_id = 'service_qf2291o'
+        const template_id = 'template_5gl6pu8'
+        const user_id = 'yzxi4uak0C0F31Ry-'
+
+        const data = {
+            service_id: service_id,
+            template_id: template_id,
+            user_id: user_id,
+            template_params: {
+                from_name: name,
+                from_email: email,
+                to_name: 'Praneeth Kulathilaka',
+                message: message
+            }
+        };
+
+        try {
+            const res = await axios.post('https://api.emailjs.com/api/v1.0/email/send',data)
+            console.log(res.data);
+            setName('');
+            setEmail('');
+            setMessage('');
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    
   return (
     <div className='flex justify-center align-center min-h-full items-start'>
-        <form className='max-w-full'>
-            <h2 className='my-2 py-6 text-left text-4xl'>Contact Me</h2>
-            <div className='my-6'>
-                <label my-2 text-left text-2xl>Full Name</label>
-                <input type="text" placeholder='Enter your name' required/>
+        <form onSubmit={onSubmitHandler} className='w-full px-2'>
+            <h2 className='my-2 py-1 text-left text-3xl'>Contact Me</h2>
+            <div className='flex flex-col my-6 py-0'>
+                <label className='my-2 text-left text-1xl'>Full Name</label>
+                <input 
+                    className="py-2 px-2 rounded bg-white/10 text-neutral-200 placeholder-neutral-200 text-sm" 
+                    type="text" 
+                    placeholder='Enter your name'
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
+                    required/>
             </div>
-            <div className='my-6'>
-                <label my-2 text-left text-2xl>Email</label>
-                <input type="text" placeholder='Enter your email' required/>
+            <div className='flex flex-col my-6'>
+                <label className='my-2 text-left text-1xl'>Email</label>
+                <input 
+                    className='py-2 px-2 rounded bg-white/10 text-neutral-200 placeholder-neutral-200 text-sm' 
+                    type="text" 
+                    placeholder='Enter your email' 
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                    required/>
             </div>
-            <div className='my-6'>
-                <label my-2 text-left text-2xl>Message</label>
-                <textarea name="" id="" placeholder='Enter description'></textarea>
+            <div className='flex flex-col my-6'>
+                <label className='my-2 text-left text-1xl'>Message</label>
+                <textarea 
+                    className='py-2 px-2 rounded bg-white/10 text-neutral-200 placeholder-neutral-200 text-sm' 
+                    name="" 
+                    id="" 
+                    placeholder='Enter description' 
+                    rows='5'
+                    value={message}
+                    onChange={(e)=>setMessage(e.target.value)}></textarea>
             </div>
-            <button type='submit'>Send</button>
+            <button className='bg-transparent hover:bg-white/10 text-neutral-400 font-semibold hover:text-neutral-200 py-2 px-4 border border-neutral-900 hover:border-transparent rounded' type='submit'>Send</button>
         </form>
     </div>
   )
